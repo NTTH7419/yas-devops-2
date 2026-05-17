31239fd07d5ead3bfda72eceb086a23faf4509f7
HB4305 <hoaichaobai@gmail.com>
2026-05-17 09:51:33 +0000

    fix(search): force ES8 headers via HTTP interceptor instead of withDefaultHeaders

--- search/src/main/java/com/yas/search/config/ImperativeClientConfig.java
+++ search/src/main/java/com/yas/search/config/ImperativeClientConfig.java
@@ -1,11 +1,12 @@
 package com.yas.search.config;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.hc.core5.http.HttpRequestInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchClients;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.support.HttpHeaders;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
@@ -18,14 +19,24 @@ public class ImperativeClientConfig extends ElasticsearchConfiguration {
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Accept", "application/vnd.elasticsearch+json;compatible-with=8");
-        headers.add("Content-Type", "application/vnd.elasticsearch+json;compatible-with=8");
-
         return ClientConfiguration.builder()
                 .connectedTo(elasticsearchConfig.getUrl())
                 .withBasicAuth(elasticsearchConfig.getUsername(), elasticsearchConfig.getPassword())
-                .withDefaultHeaders(headers)
+                .withClientConfigurer(
+                    ElasticsearchClients.ElasticsearchHttpClientConfigurationCallback.from(
+                        httpClientBuilder -> {
+                            httpClientBuilder.addInterceptorLast(
+                                (HttpRequestInterceptor) (request, entity, context) -> {
+                                    request.setHeader("Content-Type",
+                                        "application/vnd.elasticsearch+json;compatible-with=8");
+                                    request.setHeader("Accept",
+                                        "application/vnd.elasticsearch+json;compatible-with=8");
+                                }
+                            );
+                            return httpClientBuilder;
+                        }
+                    )
+                )
                 .build();
     }
 }
